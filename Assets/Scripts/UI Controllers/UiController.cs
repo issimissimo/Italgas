9ef2f293759a2e7f6df246b632c2258f956361d8
issimissimo <d.suppo@issimissimo.com>
2024-06-06 01:44:52 +0200

    OK OK

--- Assets/Scripts/UI Controllers/UiController.cs
+++ Assets/Scripts/UI Controllers/UiController.cs
@@ -5,7 +5,7 @@ using System.Collections;
 public class UiController : MonoBehaviour
 {
     public enum STATE { WAITING_FOR_PLAYERS, READY_TO_START, IN_GAME, FINAL_SCORE }
-    public enum RUNNING_STATE { NONE, OPEN_CHAPTER, OPEN_PAGE, ANSWER_CLICKED, WAIT_OTHER_PLAYER, CLOSE_PAGE }
+    public enum RUNNING_STATE { OPEN_CHAPTER, OPEN_PAGE, ANSWER_CLICKED, WAIT_OTHER_PLAYER, CLOSE_PAGE }
     public STATE state { get; private set; }
     public RUNNING_STATE runningState { get; private set; }
 
@@ -34,26 +34,13 @@ public class UiController : MonoBehaviour
 
     private IEnumerator Set(STATE? newState = null, RUNNING_STATE? newRunningState = null, bool closeAnimations = true, Action callback = null)
     {
-        if (newState != null)
-        {
-            state = newState.Value;
-            print("========> STATE: " + state.ToString());
-        }
+        if (newState != null) state = newState.Value;
+        if (newRunningState != null) runningState = newRunningState.Value;
+       
 
-        if (newRunningState != null)
-        {
-            runningState = newRunningState.Value;
-            print("========> RUNNING_STATE: " + runningState.ToString());
-        }
-
-        /// <summary>
-        /// il problema è che viene chiamato Set a brevissima distanza, quindi entrano entrambe le chiamate nello yied, e vengono chiamati alla fine 2 volte...
-        /// </summary>
-        /// <value></value>
         if (_activePanel != null && closeAnimations){
             yield return _activePanel.CloseAllAnimatedElements();
         }
-            
 
         foreach (var p in _panels)
         {
@@ -65,122 +52,6 @@ public class UiController : MonoBehaviour
             else p.GetComponent<CanvasController>().SetOff();
         }
         
-        
         _activePanel.SetupUI(state, newRunningState, callback);
     }
-
-
-
-
-
-
-    // private IEnumerator SetStateCoroutine(STATE newState)
-    // {
-    //     /// Set new state
-    //     state = newState;
-    //     print("========> STATE: " + state.ToString());
-
-
-
-
-    //     if (_activePanel != null)
-    //         yield return _activePanel.CloseAllAnimatedElements();
-
-
-    //     //  Animations.instance.ExitAllAnimations();
-    //     //     Lottie.instance.FadeOut(_activePanel._lottieAnimations, 1f);
-
-    //     // while (Animations.instance.IsAnyAnimationNotInEmptyState() || Lottie.instance.isFading)
-    //     //     yield return null;
-
-    //     // /// EXIT all animations before to proceed to new state
-    //     // yield return Animations.instance.ExitAllAnimationsAndWaitForFinish();
-
-
-
-
-    //     Set_PanelsUI_on_STATE();
-
-    //     // /// ENTER new animations
-    //     // if (UpdateAnimationsOnStateChange != null) UpdateAnimationsOnStateChange(state);
-    // }
-
-
-    // private IEnumerator SetRunningStateCoroutine(RUNNING_STATE newRunningState, bool closeAnimations = true, Action callback = null)
-    // {
-    //     /// Set new running state
-    //     runningState = newRunningState;
-    //     print("========> RUNNING_STATE: " + runningState.ToString());
-
-    //     if (closeAnimations && _activePanel != null)
-    //         yield return _activePanel.CloseAllAnimatedElements();
-
-
-
-    //     // {
-    //     // if (_activePanel != null)
-    //     // {
-
-    //     // }
-
-    //     // Animations.instance.ExitAllAnimations();
-
-    //     // if (_activePanel != null)
-    //     //     Lottie.instance.FadeOut(_activePanel._lottieAnimations, 1f);
-
-    //     // while (Animations.instance.IsAnyAnimationNotInEmptyState() || Lottie.instance.isFading)
-    //     //     yield return null;
-
-
-    //     // /// EXIT all animations that are not in EMPTY state before to proceed to new state
-    //     // yield return Animations.instance.ExitAllAnimationsAndWaitForFinish();
-
-
-    //     // }
-
-
-    //     /// Setup the UI of the new panel
-    //     Set_PanelsUI_on_RUNNING_STATE(callback);
-
-
-    //     // /// ENTER new animations
-    //     // if (UpdateAnimationsOnRunningStateChange != null) UpdateAnimationsOnRunningStateChange(runningState);
-    // }
-
-
-    // private void Set_PanelsUI_on_STATE()
-    // {
-    //     // foreach (var panel in _uiPanels)
-    //     // {
-    //     //     var panelController = panel.gameObject.GetComponent<GamePanelSubControllerBase>();
-
-    //     //     if (panelController.STATE == state)
-    //     //     {
-    //     //         _activePanel = panelController;
-    //     //         panel.SetOn();
-    //     //         panelController.SetUI_on_STATE();
-    //     //     }
-    //     //     else
-    //     //     {
-    //     //         panel.SetOff();
-    //     //     }
-    //     // }
-    // }
-
-    // private void Set_PanelsUI_on_RUNNING_STATE(Action callback = null)
-    // {
-    //     // foreach (var panel in _uiPanels)
-    //     // {
-    //     //     var panelController = panel.GetComponent<GamePanelSubControllerBase>();
-
-    //     //     if (panelController.STATE == state)
-    //     //     {
-    //     //         _activePanel = panelController;
-    //     //         panelController.SetUI_on_RUNNING_STATE(runningState, callback);
-    //     //     }
-
-    //     // }
-    // }
-
-
 }
