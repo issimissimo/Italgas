1ff76b070a2ca28af82cc410eac01de3281774bb
issimissimo <d.suppo@issimissimo.com>
2024-06-08 16:41:01 +0200

    CAAZOOO!!! ho capito perchè ne partiva solo 1.... !

--- Assets/Scripts/UI SubControllers/UiViewIntroSubController.cs
+++ Assets/Scripts/UI SubControllers/UiViewIntroSubController.cs
@@ -1,25 +1,18 @@
 using System.Collections;
 using UnityEngine;
 using System;
-using LottiePlugin.UI;
+
 
 public class UiViewIntroSubController : GamePanelSubControllerBase
 {
-    public AnimatedImage anim;
-
     public override void SetupUI(UiController.STATE state, UiController.RUNNING_STATE? runningState, Action callback)
     {
         // // if (!GameManager.instance.isDevelopment && GameManager.instance.isAppJustStarted)
-        // if (GameManager.instance.isAppJustStarted)
-        // {
-        //     GameManager.instance.isAppJustStarted = false;
-        //     StartCoroutine(ShowIntro(callback));
-        // }
-        // else callback.Invoke();
-        // StartCoroutine(ShowIntro(callback));
-
-        // if (anim != null)
-            anim.Play();
+        if (GameManager.instance.isAppJustStarted)
+        {
+            StartCoroutine(ShowIntro(callback));
+        }
+        else callback.Invoke();
     }
 
     private IEnumerator ShowIntro(Action callback)
@@ -28,19 +21,9 @@ public class UiViewIntroSubController : GamePanelSubControllerBase
 
         /// Wait for the length of the intro file
         yield return new WaitForSeconds(animationsController.Lottie_GetDuration_ByName("Intro"));
-        print("FINITO");
+
+        GameManager.instance.isAppJustStarted = false;
 
         callback.Invoke();
     }
-
-    // void Update()
-    // {
-    //     if (Input.GetKeyDown(KeyCode.E))
-    //     {
-    //         // Color c = new Color(1,1,1,0.5f);
-    //         // mat.SetColor("_Color", c);
-
-    //         anim.Play();
-    //     }
-    // }
 }
