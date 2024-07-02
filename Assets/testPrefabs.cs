127b50a1e997cb24b07b5b0b77ebf910d82998fb
issimissimo <d.suppo@issimissimo.com>
2024-07-02 19:03:37 +0200

    wip sul prefab Ui_Play_FInal_Score

---
+++ Assets/testPrefabs.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp.Net;
+using System;
+
+public class testPrefabs : MonoBehaviour
+{
+    [Serializable]
+    public class TestKey
+    {
+        public KeyCode keyCode;
+        public string animationName;
+    }
+
+    [Serializable]
+    public class Animation
+    {
+        public AnimationsController animationsController;
+        public TestKey[] testKeys;
+    }
+
+
+    public Animation[] animations;
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
+            {
+                if (Input.GetKey(kcode))
+                {
+                    Debug.Log("KeyCode down: " + kcode);
+
+                    foreach (var a in animations)
+                    {
+                        foreach (var t in a.testKeys)
+                        {
+                            if (t.keyCode == kcode)
+                            {
+                                a.animationsController.Tween_PlayByName(t.animationName);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+
+
+
+        // if (Input.GetKeyDown(KeyCode.E))
+        // {
+        //     animationsController.Tween_PlayByName("[ENTER WINNER]");
+        // }
+    }
+}
